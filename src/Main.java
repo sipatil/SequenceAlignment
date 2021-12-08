a516aa9c01bf6163188bb2180371e0a90122a5ef
sipatil <94469232+sipatil@users.noreply.github.com>
2021-12-08 14:13:39 -0800

    problem size = m+n

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
             InputGenerator inputGenerator = new InputGenerator(inputFilePath);
             String x = inputGenerator.getX();
             String y = inputGenerator.getY();
-            LOGGER.log(Level.INFO, "Problem size: " + x.length() * y.length());
+            LOGGER.log(Level.INFO, "Problem size: " + x.length() + y.length());
 
             FileWriter fileWriter = createOutputFile();
             runSequenceAlignmentAlgo(x, y, fileWriter);
